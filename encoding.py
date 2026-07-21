d80b1fe81cb3e4aadaf64afda2f8d961dd01edcb
abe-narayan <115516335+abe-narayan@users.noreply.github.com>
2026-07-21 13:00:19 -0700

    Refactor DIRECTIONS and OPPOSITE for 3D coordinates

--- encoding.py
+++ encoding.py
@@ -1,17 +1,15 @@
 DIRECTIONS = {
-
-(0,0):(1,0),
-(0,1):(0,1),
-(1,0):(-1,0),
-(1,1):(0,-1),
+    (0, 0): (1, 1, 1),
+    (0, 1): (1, -1, -1),
+    (1, 0): (-1, 1, -1),
+    (1, 1): (-1, -1, 1),
 }
 
 OPPOSITE = {
-
-(0,0):(1,0),
-(1,0):(0,0),
-(0,1):(1,1),
-(1,1):(0,1),
+    (0, 0): (0, 0),
+    (0, 1): (0, 1),
+    (1, 0): (1, 0),
+    (1, 1): (1, 1),
 }
 
 
@@ -19,11 +17,11 @@ def bits_to_directions(bitstring):
     return [(int(bitstring[2*i]), int(bitstring[2*i+1])) for i in range(len(bitstring) // 2)]
  
 def bits_to_coords(bitstring):
-    coords = [(0, 0)]
-    x, y = 0, 0
+    coords = [(0, 0, 0)]
+    x, y, z = 0, 0, 0
     for (b0, b1) in bits_to_directions(bitstring):
-        dx, dy = DIRECTIONS[(b0, b1)]
-        x, y = x + dx, y + dy
-        coords.append((x, y))
+    dx, dy, dz = DIRECTIONS[(b0, b1)]
+    x, y, z = x + dx, y + dy, z + dz
+    coords.append((x, y, z))
     return coords
 
