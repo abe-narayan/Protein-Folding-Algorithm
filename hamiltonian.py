c2d4b1f7f87b7d4d5402e3c9f7d972452a102d52
abe-narayan <115516335+abe-narayan@users.noreply.github.com>
2026-07-21 16:54:42 -0700

    Refactor path energy calculation into path hamiltonian

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,7 +1,6 @@
 from encoding import bits_to_coords, bits_to_directions, DIRECTIONS
-import cirq as cirq
 import matplotlib.pyplot as plt
-
+from local_hamiltonian import build_local_hamiltonian
 
 INTERACTIONS = {
     ('H', 'H'): -1.0, ('H', 'P'):  0.0, ('H', '+'):  0.0, ('H', '-'):  0.0,
@@ -19,32 +18,5 @@ def get_interaction(a, b):
     return FALLBACK_INTERACTION
 
 
-
-def path_energy(bitstring, sequence, overlap_penalty=10.0):
-    coords = bits_to_coords(bitstring)
-    dirs = bits_to_directions(bitstring)
-    energy = 0.0
-    n_residues = len(sequence)
-    expected_bits = 2 * (n_residues - 1)
-
-    for i in range(n_residues):
-        for j in range(i + 1, n_residues):
-            if coords[i] == coords[j]:
-                energy += overlap_penalty
-
-    for i in range(n_residues):
-        for j in range(i + 2, n_residues):
-            dx = coords[i][0] - coords[j][0]
-            dy = coords[i][1] - coords[j][1]
-            dz = coords[i][2] - coords[j][2]
-
-            if dx * dx + dy * dy + dz * dz == 8:
-                energy += get_interaction(sequence[i], sequence[j])
-
-    if len(bitstring) != expected_bits:
-        raise ValueError(
-            f"Expected {expected_bits} bits for {len(sequence)} residues, "
-            f"but received {len(bitstring)}"
-        )
-
-    return energy
+def path_hamiltonian(sequence, overlap_penalty=10.0):
+    return build_local_hamiltonian(sequence, get_interaction, overlap_penalty)
