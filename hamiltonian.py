f721998b3da6f028929e3a5bdd560650dfb9e185
abe-narayan <115516335+abe-narayan@users.noreply.github.com>
2026-07-21 12:42:08 -0700

    Remove bits_to_directions and bits_to_coords functions
    Removed unused functions for converting bits to directions and coordinates.

--- hamiltonian.py
+++ hamiltonian.py
@@ -38,19 +38,6 @@ n_turns = n_residues - 1
 n_qubits = 2 * n_turns
 
 
-def bits_to_directions(bitstring):
-    return [(int(bitstring[2*i]), int(bitstring[2*i+1])) for i in range(n_turns)]
- 
-def bits_to_coords(bitstring):
-    coords = [(0, 0)]
-    x, y = 0, 0
-    for (b0, b1) in bits_to_directions(bitstring):
-        dx, dy = DIRECTIONS[(b0, b1)]
-        x, y = x + dx, y + dy
-        coords.append((x, y))
-    return coords
-
-
 def path_energy(bitstring, overlap_penalty=10.0, reversal_penalty=5.0):
     coords = bits_to_coords(bitstring)
     dirs = bits_to_directions(bitstring)
