f92c041e46a7eac4736e670b02751c63c4df7a40
Leo Denizen <leo@denizen-family.com>
2026-07-21 22:40:43 +0200

    simple comments

--- classical.py
+++ classical.py
@@ -7,6 +7,7 @@ seq = ['-', 'P','H','+'] #adjustable
 n_turns = len(seq)-1
 n_qubits = 2*n_turns
 
+#Brute force classical algorithm that searches through every single bitstring possibility
 energy_list = []
 for idx in range(2 ** n_qubits):
     bitstring = format(idx, f'0{n_qubits}b')
@@ -14,6 +15,7 @@ for idx in range(2 ** n_qubits):
     energy_list.append(energy)
 energy_table = numpy.array(energy_list)
 
+#Finds index of lowest energy total from above
 idx = energy_table.argmin()
 lowest_energy = energy_table[idx]
 best_bitstring = format(idx, f'0{n_qubits}b')
