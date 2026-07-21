edb0fd5ef3ae62d5cc538dea872982baa838d601
Leo Denizen <leo@denizen-family.com>
2026-07-21 21:21:43 +0200

    not calculated yet

--- classical.py
+++ classical.py
@@ -8,3 +8,25 @@ energy_interactions = {
     ('-','+'):-placeholder,
     ('H', 'H'): -placeholder
 }
+
+DIRECTIONS = {
+    (0, 0): (1, 0),   # +x
+    (0, 1): (0, 1),   # +y
+    (1, 0): (-1, 0),  # -x
+    (1, 1): (0, -1),  # -y
+}
+OPPOSITE = {
+    (0, 0): (1, 0),
+    (1, 0): (0, 0),
+    (0, 1): (1, 1),
+    (1, 1): (0, 1),
+}
+
+lambda_back = 1000
+def turn_calculation(aminos, turns):
+    classical_energy = 0
+    for i in range(0, len(turns)-1):
+        if turns[i+1] == OPPOSITE[turns[i]]:
+            classical_energy += lambda_back
+    for j in range(0, len(turns)-2):
+        if
