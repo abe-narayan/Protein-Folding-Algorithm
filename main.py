6f1a6f4e09ec2a1037993002b32ae96a0bbdd1b2
abe-narayan <Abez.narayan@gmail.com>
2026-07-21 17:39:34 -0700

    main.py edit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from hamiltonian import path_hamiltonian
+from local_hamiltonian import build_cost_function
 
-sequence = "-HP+H-P-H"
 
-H = path_hamiltonian(sequence)
+sequence = "HP+-HHP+-HHP+-"
 
-print(H)
+
+cost_function = build_cost_function(sequence)
+
+
+bitstring = "0000000000000000000000000000"
+
+
+energy = cost_function(bitstring)
+
+
+print("Sequence:", sequence)
+print("Number of amino acids:", len(sequence))
+print("Number of qubits:", 2 * (len(sequence) - 1))
+print("Bitstring:", bitstring)
+print("Energy:", energy)
