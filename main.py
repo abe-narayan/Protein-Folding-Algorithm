ab0bb0416acf661f827083849ce3b17a0f659eb3
abe-narayan <Abez.narayan@gmail.com>
2026-07-21 18:08:35 -0700

    make main better/easier to compute

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from vqe import run_vqe
 
 
-sequence = "HP+-H"
+sequence = "HP+-H--+-PPH+H"
 
 
 result, history = run_vqe(
     sequence=sequence,
     alpha=0.5,
-    repetitions=1000,
-    optimization_steps=50
+    repetitions=100,
+    optimization_steps=10
 )
 
 
